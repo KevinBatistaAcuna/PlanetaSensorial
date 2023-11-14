afe7e4df1c04694797d10dcd41e25b96ee8e97c1
Josestate <josestate19@gmail.com>
2023-11-14 09:56:51 -0600

    putting images to lists

--- src/components/Lists/Lists.jsx
+++ src/components/Lists/Lists.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import './Lists.css'
 
-export default function Lists({title, content, classSelected}) {
+export default function Lists({title, content, classSelected, img, orientationImg}) {
   return (
+    
     <section className={`listComp ${classSelected}`}>
-      <h2>{title}</h2>
-      <ul className='listContent'>
-          {content.map(item => {
-              return <li key={item}>{item}</li>
-          })}
-      </ul>
+    {orientationImg == 'left' && <img src={img} alt="Image" className='back'/>}
+      <div className='textCont'>
+        <h2>{title}</h2>
+        <ul className='listContent'>
+            {content.map(item => {
+                return (
+                <> 
+                  <li key={item}>{item}</li>
+                </> 
+                )
+            })}
+        </ul>
+      </div>
+      {orientationImg == 'right' && <img src={img} alt="Image" className='back'/>}
     </section>
   )
 }
