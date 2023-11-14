9d546f029a79d73108bab356dc9caf77e8e973f7
Kevin <kbatista23@gmail.com>
2023-11-14 08:32:06 -0600

    conozcanos agregando gradientes

--- src/components/Lists/Lists.jsx
+++ src/components/Lists/Lists.jsx
@@ -4,12 +4,12 @@ import './Lists.css'
 export default function Lists({title, content, classSelected}) {
   return (
     <section className={`listComp ${classSelected}`}>
-        <h2>{title}</h2>
-        <ul className='listContent'>
-            {content.map(item => {
-                return <li key={item}>{item}</li>
-            })}
-        </ul>
+      <h2>{title}</h2>
+      <ul className='listContent'>
+          {content.map(item => {
+              return <li key={item}>{item}</li>
+          })}
+      </ul>
     </section>
   )
 }
