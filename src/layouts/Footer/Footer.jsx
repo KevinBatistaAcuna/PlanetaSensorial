7e4c0bbfd019416ccb9c6b52d659f248d4e7dfb7
Kevin <kbatista23@gmail.com>
2023-11-17 13:18:55 -0600

    working on social media items

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -4,16 +4,15 @@ import './Footer.css'
 function Footer() {
   return (
     <footer>
-        <h2>@PlanetaSensorial</h2>
-        <nav>
-            <ul>
-                <li>somos una clinica blablabla</li>
-                <li><a>instagram</a></li>
-                <li><a>Facebook</a></li>
-                <li><a>Whatssap</a></li>
-                <li>6600 7890</li>
-            </ul>
-        </nav>
+      <div className='imgContainer'>
+        <img src="src\assets\logo.png" alt="logo" className='logoImg'/>
+      </div>
+      <p>© 2023 PlanetaSensorial. Todos los derechos reservados</p>
+      <nav>
+        <a href="https://www.facebook.com/planetasensorialcr?mibextid=ZbWKwL" target='__blank'><img src="src\assets\Facebook_Logo.png" alt="" className='redesImg'/></a>
+        <a href="https://instagram.com/planetasensorialcr?igshid=MTNiYzNiMzkwZA==" target='__blank'><img src="src\assets\instagram-logo.png" alt="" className='redesImg'/></a>
+        <a href="https://api.whatsapp.com/send?phone=50672438091" target='__blank'><img src="src\assets\whatsapp-logo.png" alt="" className='redesImg'/></a>
+      </nav>
     </footer>
   )
 }
