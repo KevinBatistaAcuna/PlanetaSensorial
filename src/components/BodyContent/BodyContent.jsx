86199906497970723979964ac89a5aea0dd99364
Kevin <kbatista23@gmail.com>
2023-11-17 12:11:25 -0600

    creando heroComp para conozcanos

--- src/components/BodyContent/BodyContent.jsx
+++ src/components/BodyContent/BodyContent.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import './BodyContent.css'
+import img from '../../assets/Planetasensorialimg3.jpg'
+import HeroComp from '../HeroComp/HeroComp'
 
 export default function BodyContent() {
   return (
-    <div className='divBody'>
-        <section>
-            <h2>Bienvenido</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi illum amet dolorum molestias vel beatae dolore, a tempore sed sequi quam dignissimos? Quaerat corrupti ad quidem deserunt facilis rerum reiciendis.</p>
-        </section>
-    </div>
+    <HeroComp image={img} title={'Bienvedido'} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, sapiente? Ullam at nam harum dignissimos, ex consectetur alias eius officiis, fugit ad velit. Sed quo sit est aspernatur possimus sunt!'}/>
   )
 }
--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -1,9 +1,10 @@
 import {React, useState, useRef} from 'react'
 import ContactNumbers from '../../components/ContactNumbers/ContactNumbers'
 import './Contacto.css'
-import HeroContact from '../../components/HeroContact/HeroContact';
-import InputComps from '../../components/InputComps/InputComp'
+import HeroComp from '../../components/HeroComp/HeroComp';
+import InputComps from '../../components/InputComps/InputComp';
 import MessageComp from '../../components/MessageComp/MessageComp';
+import img from '../../assets/Planetasensorialimg5.jpg'
 
 export default function Contact() {
   let [val, setVal] = useState();
@@ -14,7 +15,7 @@ export default function Contact() {
 
   return (
     <div className='contactBody'>
-      <HeroContact />
+      <HeroComp image={img} title={'Contactenos'} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, sapiente? Ullam at nam harum dignissimos, ex consectetur alias eius officiis, fugit ad velit. Sed quo sit est aspernatur possimus sunt!'}/>
       <div className='contactBody__contactMessage'>
         <section className='sectionNums__formMessage'>
           <ContactNumbers/>
