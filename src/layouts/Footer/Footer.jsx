24699c8f8efe55b66b1e1a321522d4997cc888f6
Kevin <kbatista23@gmail.com>
2023-11-20 16:51:27 -0600

    importando imagenes

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import './Footer.css'
+import logo from '../../assets/logo.png'
+import face from '../../assets/Facebook_Logo.png'
+import insta from '../../assets/instagram-logo.png'
+import whatsapp from '../../assets/whatsapp-logo.png'
 
 function Footer() {
   return (
     <footer>
       <div className='imgContainer'>
-        <img src="src\assets\logo.png" alt="logo" className='logoImg'/>
+        <img src={logo} alt="logo" className='logoImg'/>
       </div>
       <p>© 2023 PlanetaSensorial. Todos los derechos reservados</p>
       <nav>
-        <a href="https://www.facebook.com/planetasensorialcr?mibextid=ZbWKwL" target='__blank'><img src="src\assets\Facebook_Logo.png" alt="" className='redesImg'/></a>
-        <a href="https://instagram.com/planetasensorialcr?igshid=MTNiYzNiMzkwZA==" target='__blank'><img src="src\assets\instagram-logo.png" alt="" className='redesImg'/></a>
-        <a href="https://api.whatsapp.com/send?phone=50683185409" target='__blank'><img src="src\assets\whatsapp-logo.png" alt="" className='redesImg'/></a>
+        <a href="https://www.facebook.com/planetasensorialcr?mibextid=ZbWKwL" target='__blank'><img src={face} alt="" className='redesImg'/></a>
+        <a href="https://instagram.com/planetasensorialcr?igshid=MTNiYzNiMzkwZA==" target='__blank'><img src={insta} alt="" className='redesImg'/></a>
+        <a href="https://api.whatsapp.com/send?phone=50672438091" target='__blank'><img src={whatsapp} alt="" className='redesImg'/></a>
       </nav>
     </footer>
   )
