5d77b21de3d03ad99b56c48c92d53e4cdff5d047
Kevin <kbatista23@gmail.com>
2023-11-20 16:35:45 -0600

    cambiando numero

--- src/components/ContactNumbers/ContactNumbers.jsx
+++ src/components/ContactNumbers/ContactNumbers.jsx
@@ -14,7 +14,7 @@ export default function ContactNumbers() {
             <div className='redesContainer'>
               <a href="https://instagram.com/planetasensorialcr?igshid=MTNiYzNiMzkwZA==" target='__blank'><img src='src\assets\instagram-logo.png' className='redesImages' alt='instagram'/></a>
               <a href="https://www.facebook.com/planetasensorialcr" target='__blank'><img src='src\assets\Facebook_Logo.png' className='redesImages' alt='facebook'/></a>
-              <a href="https://api.whatsapp.com/send?phone=50672438091" target='__blank'><img src='src\assets\whatsapp-logo.png' className='redesImages' alt='whatsap'/></a>
+              <a href="https://api.whatsapp.com/send?phone=50683185409" target='__blank'><img src='src\assets\whatsapp-logo.png' className='redesImages' alt='whatsap'/></a>
             </div>
           </div> 
         <div className='ubication'><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=es&amp;q=Les%20Rambles,%201%20Barcelona,%20Spain+(Mi%20nombre%20de%20egocios)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/car-satnav-gps/">GPS car tracker</a></iframe></div>
--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
       <nav>
         <a href="https://www.facebook.com/planetasensorialcr?mibextid=ZbWKwL" target='__blank'><img src="src\assets\Facebook_Logo.png" alt="" className='redesImg'/></a>
         <a href="https://instagram.com/planetasensorialcr?igshid=MTNiYzNiMzkwZA==" target='__blank'><img src="src\assets\instagram-logo.png" alt="" className='redesImg'/></a>
-        <a href="https://api.whatsapp.com/send?phone=50672438091" target='__blank'><img src="src\assets\whatsapp-logo.png" alt="" className='redesImg'/></a>
+        <a href="https://api.whatsapp.com/send?phone=50683185409" target='__blank'><img src="src\assets\whatsapp-logo.png" alt="" className='redesImg'/></a>
       </nav>
     </footer>
   )
