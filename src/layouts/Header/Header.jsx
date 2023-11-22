5ae1d86daf213eb7c3e0795af6e31cfe69c91c41
Kevin <kbatista23@gmail.com>
2023-11-22 10:45:34 -0600

    creating menu button

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import {React, useState} from 'react'
 import logo from '../../assets/logo.png'
 import './Header.css'
 import { Link } from "react-router-dom";
+import img from '../../assets/Hamburger_icon.svg.png'
 
 const Header = () => {
+  const [visibleNav, setVisibleNav] = useState(false)
   return (
     <header className='header'>
       <Link to="/">
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" className='logoImg'/>
       </Link>
-      <nav>
-        <ul>
-          <li><Link to="/">Inicio</Link></li>
-          <li><Link to="/nosotros">Nosotros</Link></li>
-          <li><Link to="/contacto">Contacto</Link></li>
-          <li><Link to="/servicios">Servicios</Link></li>
-        </ul>
-      </nav>
+      <section className='navSection'>
+        {window.screen.width < 770 && <img src={img} alt='menu' className='menuImg' onClick={()=> setVisibleNav(!visibleNav)}/>}
+        <nav className={visibleNav ? 'visibleNav' : 'notVisibleNav'}>
+          <ul>
+            <li><Link to="/">Inicio</Link></li>
+            <li><Link to="/nosotros">Nosotros</Link></li>
+            <li><Link to="/contacto">Contacto</Link></li>
+            <li><Link to="/servicios">Servicios</Link></li>
+          </ul>
+        </nav>
+      </section>
     </header>
   )
 }
