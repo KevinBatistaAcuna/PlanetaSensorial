cf4692ceef0c96802e6f9d22e52700754539d785
Kevin <kbatista23@gmail.com>
2023-11-22 09:58:32 -0600

    removing serviceType

--- src/pages/Servicios/Servicios.jsx
+++ src/pages/Servicios/Servicios.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import HeroComp from '../../components/HeroComp/HeroComp'
 import imgHero from '../../assets/Planetasensorialimg6.jpg'
 import imgTerapia from '../../assets/Terapia-fisica-ninos-pediatra.jpg'
-import ServicioType from '../../components/ServicioType/ServicioType'
+import TextImg from '../../components/TextImg/TextImg'
 
 export default function Servicios() {
   return (
     <div>
         <HeroComp image={imgHero} title={'Servicios'} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae maxime reiciendis necessitatibus, ab nemo repellat sit quas earum eligendi, numquam impedit expedita aperiam perspiciatis, minima quaerat obcaecati eius beatae veritatis!'}/>
         <section>
-            <ServicioType orientationImg={window.screen.width < 770 ? 'left' : 'right'} image={imgTerapia} title={'Terapia fisica'} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae maxime reiciendis necessitatibus, ab nemo repellat sit quas earum eligendi, numquam impedit expedita aperiam perspiciatis, minima quaerat obcaecati eius beatae veritatis!'}/>
-            <ServicioType light={true} orientationImg={'left'} image={imgTerapia} title={'Terapia fisica'} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae maxime reiciendis necessitatibus, ab nemo repellat sit quas earum eligendi, numquam impedit expedita aperiam perspiciatis, minima quaerat obcaecati eius beatae veritatis!'}/>
-            <ServicioType orientationImg={window.screen.width < 770 ? 'left' : 'right'} image={imgTerapia} title={'Terapia fisica'} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae maxime reiciendis necessitatibus, ab nemo repellat sit quas earum eligendi, numquam impedit expedita aperiam perspiciatis, minima quaerat obcaecati eius beatae veritatis!'}/>
-            <ServicioType light={true} orientationImg={'left'} image={imgTerapia} title={'Terapia fisica'} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae maxime reiciendis necessitatibus, ab nemo repellat sit quas earum eligendi, numquam impedit expedita aperiam perspiciatis, minima quaerat obcaecati eius beatae veritatis!'}/>
+            <TextImg classSelected={'noLight'} orientationImg={window.screen.width < 770 ? 'left' : 'right'} img={imgTerapia} title='Terapia fisica' content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae maxime reiciendis necessitatibus, ab nemo repellat sit quas earum eligendi, numquam impedit expedita aperiam perspiciatis, minima quaerat obcaecati eius beatae veritatis!'/>
+            <TextImg classSelected={'light'} orientationImg={'left'} img={imgTerapia} title='Terapia fisica' content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae maxime reiciendis necessitatibus, ab nemo repellat sit quas earum eligendi, numquam impedit expedita aperiam perspiciatis, minima quaerat obcaecati eius beatae veritatis!'/>
+            <TextImg classSelected={'noLight'} orientationImg={window.screen.width < 770 ? 'left' : 'right'} img={imgTerapia} title='Terapia fisica' content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae maxime reiciendis necessitatibus, ab nemo repellat sit quas earum eligendi, numquam impedit expedita aperiam perspiciatis, minima quaerat obcaecati eius beatae veritatis!'/>
+            <TextImg classSelected={'light'} orientationImg={'left'} img={imgTerapia} title='Terapia fisica' content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae maxime reiciendis necessitatibus, ab nemo repellat sit quas earum eligendi, numquam impedit expedita aperiam perspiciatis, minima quaerat obcaecati eius beatae veritatis!'/>
         </section>
     </div>
   )
