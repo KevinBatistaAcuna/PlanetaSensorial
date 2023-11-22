f59cc9b25981351528f1a7a9b98517f4e416e40d
Kevin <kbatista23@gmail.com>
2023-11-22 12:45:57 -0600

    making border to images and making link ver mas

--- src/components/TextImg/TextImg.jsx
+++ src/components/TextImg/TextImg.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './TextImg.css'
 
-export default function TextImg({title, content, img, classSelected, orientationImg, borderRadius}) {
+export default function TextImg({title, content, img, classSelected, orientationImg, borderRadius, clickable}) {
   return (
     <section className={`textImg ${classSelected}`}>
       {orientationImg == 'left' && <img src={img} alt="Image" style={{borderRadius: borderRadius}} className='back'/>}
       <div className='content'>
           <h2>{title}</h2>
           <p>{content}</p>
+          {clickable && <a id='#'  style={{cursor: 'pointer'}}>Ver mas</a>}
       </div>
       {orientationImg == 'right' && <img src={img} alt="Image" style={{borderRadius: borderRadius}} className='back'/>}
     </section>
