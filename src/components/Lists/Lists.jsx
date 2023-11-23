c23bfc8134cde77b5fcb421990dba74576036950
Josestate <josestate19@gmail.com>
2023-11-22 21:43:18 -0600

    changin images styles

--- src/components/Lists/Lists.jsx
+++ src/components/Lists/Lists.jsx
@@ -5,7 +5,7 @@ export default function Lists({title, content, classSelected, img, orientationIm
   return (
     
     <section className={`listComp ${classSelected}`}>
-    {orientationImg == 'left' && <img src={img} alt="Image" style={{borderRadius: borderRadius}} className='back'/>}
+    {orientationImg == 'left' && <img src={img} alt="Image" style={window.screen.width < 768? {borderRadius:'unset'} : {borderRadius: borderRadius}} className='back'/>}
       <div className='textCont'>
         <h2>{title}</h2>
         <ul className='listContent'>
@@ -18,7 +18,7 @@ export default function Lists({title, content, classSelected, img, orientationIm
             })}
         </ul>
       </div>
-      {orientationImg == 'right' && <img src={img} alt="Image" style={{borderRadius: borderRadius}} className='back'/>}
+      {orientationImg == 'right' && <img src={img} alt="Image" style={window.screen.width < 768? {borderRadius:'unset'} : {borderRadius: borderRadius}} className='back'/>}
     </section>
   )
 }
--- src/components/TextImg/TextImg.jsx
+++ src/components/TextImg/TextImg.jsx
@@ -5,13 +5,13 @@ import { Link } from "react-router-dom";
 export default function TextImg({title, content, img, classSelected, orientationImg, borderRadius, clickable, route}) {
   return (
     <section className={`textImg ${classSelected}`}>
-      {orientationImg == 'left' && <img src={img} alt="Image" style={{borderRadius: borderRadius}} className='back'/>}
+      {orientationImg == 'left' && <img src={img} alt="Image" style={window.screen.width < 768? {borderRadius:'unset'} : {borderRadius: borderRadius}} className='back'/>}
       <div className='content'>
           <h2>{title}</h2>
           <p>{content} {clickable && <Link className='link' to={`/${route}`}>Ver más...</Link>}</p>
           
       </div>
-      {orientationImg == 'right' && <img src={img} alt="Image" style={{borderRadius: borderRadius}} className='back'/>}
+      {orientationImg == 'right' && <img src={img} alt="Image" style={window.screen.width < 768? {borderRadius:'unset'} : {borderRadius: borderRadius}} className='back'/>}
     </section>
   )
 }
